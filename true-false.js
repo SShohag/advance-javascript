f3d84777d55453c29005ed777cfb00baa18ca879
SShohag <shohag7m@gmail.com>
2020-08-06 23:34:28 +0600

    First add

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//Falsy value
+0
+""
+null
+NaN
+undefined
+false
+// emapty variable
+
+//truthy value
+// '0', " ", {}, [].
+
+let name ={};
+console.log(name);
+if(name){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
